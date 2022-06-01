51278b2bfda565cd90f5784395eeb1556aaa0013
Curtis DeGasperi <curtis.degasperi@gmail.com>
2022-06-01 16:25:06 -0700

    Trying to link to github. Already many trys.

---
+++ read_DMPA.R
@@ -0,0 +1,13 @@
+require(readr)
+
+read_DMPA <- function(filename, path = '//kc.kingcounty.lcl/dnrp/LAB/Reports/',...) 
+{
+  filename = paste(path,filename,sep="")  
+  read_delim(filename, col_types = cols(LABSAMPLENUM = col_character(), 
+                                        COLLECTDATE = col_datetime(format = "%m/%d/%Y %H:%M:%S"),
+                                        TOTAL_SOLIDS = col_double(),
+                                        PROJECT = col_character(),
+                                        SAMPLE_DEPTH = col_double(),
+                                        MDL = col_double(),
+                                        RDL = col_double()), delim = "\t")
+}
